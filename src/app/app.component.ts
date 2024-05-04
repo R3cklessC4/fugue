6ee7b2fba743411947ab7c0ed408f7dfbd435343
Joshua Angulo <jangulo@albany.edu>
2024-05-03 20:17:41 -0400

    Homepage re-added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Event, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fugue';
+  imagePath: any;
+  isLogin: boolean = false;
+
+  constructor(private router: Router){
+    router.events.subscribe((event: Event) => {
+      if(event instanceof NavigationEnd) {
+        this.isLogin = event.url === '/login';
+      }
+    });
+  }
+
+  loginWithRedirect() {
+    // this.auth.loginWithRedirect;
+  }
 }
